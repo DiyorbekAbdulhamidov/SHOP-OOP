68a79f8d844863a5b5b89ba80d6dbd5d7c5ca847
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-05-02 00:55:38 +0500

    codes

--- shop/main.ts
+++ shop/main.ts
@@ -3,3 +3,10 @@ import { Product } from "./model/product/product";
 import { ProductService } from "./service/productService";
 import { UserService } from "./service/userService";
 
+class Main {
+    userService = new UserService;
+    productService = new ProductService;
+
+    
+
+}
--- shop/service/productService.ts
+++ shop/service/productService.ts
@@ -4,57 +4,57 @@ import { Phone } from "../model/product/phone";
 import { Product } from "../model/product/product";
 import { TV } from "../model/product/tv";
 
-export class ProductService {
+export class ProductService{
     productList: Product[] = [];
     private idGenerator = 0;
-
-
-    add(product: Product) {
+    
+    
+    add(product : Product){
         product.setId(++this.idGenerator);
         this.productList.push(product);
         return true;
     }
 
-    getById(id: number) {
-        for (let product of this.productList) {
-            if (product.getId() === id) {
+    getById (id : number){
+        for(let product of this.productList){
+            if(product.getId() === id){
                 return product;
             }
         }
         return null;
     }
 
-    getProductsByType(type: string) {
-        const productsOfType: Product[] = [];
+    getProductsByType (type : string){
+        const productsOfType : Product[] = [];
 
-        switch (type) {
-            case "TV": {
-                for (const product of this.productList) {
-                    if (product instanceof TV) {
+        switch(type){
+            case "TV" : {
+                for(const product of this.productList){
+                    if(product instanceof TV){
                         productsOfType.push(product)
                     }
                 }
             }
 
-            case "Laptop": {
-                for (const product of this.productList) {
-                    if (product instanceof Laptop) {
+            case "Laptop" : {
+                for(const product of this.productList){
+                    if(product instanceof Laptop){
+                        productsOfType.push(product)
+                    }
+                }
+            }
+            
+            case "Phone" : {
+                for(const product of this.productList){
+                    if(product instanceof Phone){
                         productsOfType.push(product)
                     }
                 }
             }
 
-            case "Phone": {
-                for (const product of this.productList) {
-                    if (product instanceof Phone) {
-                        productsOfType.push(product)
-                    }
-                }
-            }
-
-            case "Monitor": {
-                for (const product of this.productList) {
-                    if (product instanceof Monitor) {
+            case "Monitor" : {
+                for(const product of this.productList){
+                    if(product instanceof Monitor){
                         productsOfType.push(product)
                     }
                 }
@@ -62,4 +62,14 @@ export class ProductService {
         }
         return productsOfType;
     }
+    edit(updatedProduct:Product){
+        for (let i = 0; i < this.productList.length; i++) {
+            if(this.productList[i].getId() === updatedProduct.getId()) {
+                this.productList[i] = updatedProduct;
+                return true
+            }
+        }
+        return false;
+    }
+
 }
